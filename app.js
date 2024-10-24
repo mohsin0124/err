3db14211badfbef66d0e673ebb7d4dc2e45a8e71
Mohsin <mohsinmohammed00011@gmail.com>
2024-10-24 20:25:46 +0530

    implemented crud operations

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
     .then(()=>{
@@ -15,24 +17,78 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 };
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
 app.get("/",(req,res)=>{
     res.send("iam root");
 });
 
-app.get("/testlisting",async (req,res)=>{
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India",
-    });
-
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send(sampleListing);
-
+//index route
+app.get("/listings",async(req,res) =>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
 });
+
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+//show route
+app.get("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+//create route
+app.post("/listings",async (req,res)=>{
+    //let {title, description, image, price, country, location} = req.body;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+//update route
+app.put("/listings/:id",async (req,res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//delete route
+app.delete("/listings/:id",async (req,res) =>{
+    let {id} = req.params;
+    let delListing = await Listing.findByIdAndDelete(id);
+    console.log(delListing);
+    res.redirect("/listings");
+})
+
+
+// app.get("/testlisting",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send(sampleListing);
+
+// });
 //edge://flags/
 app.listen(8080,()=>{
     console.log("listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -8,12 +8,15 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        type: String,
+        filename: String,
+        url: {
+          type: String,
         default: "https://images.unsplash.com/photo-1483168527879-c66136b56105?q=80&w=1832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v) =>
             v === ""
                 ? "https://images.unsplash.com/photo-1483168527879-c66136b56105?q=80&w=1832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 :v,
+        },
     },
     price: Number,
     location: String,
