e7a9fe17c7ddcfef7cd75690abe7e29b647b7826
Mohsin <mohsinmohammed00011@gmail.com>
2024-11-05 01:24:30 +0530

    updated on 5nov

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -32,6 +33,17 @@ app.get("/",(req,res)=>{
     res.send("iam root");
 });
 
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    
+    if(error){
+        throw new ExpressError(400,error);
+        console.log(error);
+    } else{
+        next();
+    }
+}; 
+
 //index route
 app.get("/listings",wrapAsync(async(req,res) =>{
     const allListings = await Listing.find({});
@@ -52,12 +64,30 @@ app.get("/listings/:id",wrapAsync(async (req,res)=>{
 
 
 //create route
-app.post("/listings",wrapAsync(async (req,res,next)=>{
-    if(!req.body.listings){
-        throw new ExpressError(400,"Send valid data for listing");
+app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
+
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+        throw new ExpressError(400,result.error);
     }
-    //let {title, description, image, price, country, location} = req.body;
     const newListing = new Listing(req.body.listing);
+    
+    // if(!req.body.listing){
+    //     throw new ExpressError(400,"Send valid data for listing");
+    // }
+    //let {title, description, image, price, country, location} = req.body;
+ 
+    // if(!newListing.title){
+    //     throw new ExpressError(400,"Title is missing");
+    // }
+    // if(!newListing.description){
+    //     throw new ExpressError(400,"Description is missing");
+    // }
+    // if(!newListing.location){
+    //     throw new ExpressError(400,"Location is missing");
+    // }
+
     await newListing.save();
     res.redirect("/listings");
   
@@ -71,10 +101,10 @@ app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
 }));
 
 //update route
-app.put("/listings/:id",wrapAsync(async (req,res) => {
-    if(!req.body.listings){
-        throw new ExpressError(400,"Send valid data for listing");
-    }
+app.put("/listings/:id",validateListing,wrapAsync(async (req,res) => {
+    // if(!req.body.listings){
+    //     throw new ExpressError(400,"Send valid data for listing");
+    // }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
